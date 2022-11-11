af8d7b4ada2bd7aeddca99623f22819aab150e51
Sheuly Akter <77951733+Sheuly-Akter-04@users.noreply.github.com>
2022-11-12 03:14:43 +0600

    Add files via upload

---
+++ Author.java
@@ -0,0 +1,25 @@
+
+public class Author extends Publisher{
+    int authorId;
+    String authorName;
+    String email;
+
+    void setAuthorId(int authorId){
+        this.authorId=authorId;
+    }
+    void setAuthorName(String authorName){
+        this.authorName=authorName;
+    }
+    void setEmail(String email){
+        this.email=email;		
+    }
+    int getAuthorId(){
+        return authorId;
+    }
+    String getAuthorName(){
+        return authorName;
+    }
+    String getEmail(){
+        return email;
+    }
+}
